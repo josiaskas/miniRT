9091dbb54815e91e4fd32c7785a765bbb8edc681
Josias Kasongo <josiaskas@gmail.com>
2022-12-12 17:29:43 -0500

    start changing plan and sphere #9

--- srcs/config/select_mode.c
+++ srcs/config/select_mode.c
@@ -28,82 +28,20 @@ static inline t_hittable	*find_selected_object(t_scene	*scn, int x, int y)
 	return (NULL);
 }
 
-void	sphere_edition(t_hittable *sphere)
-{
-	t_color	color;
-	t_v3	translate;
-	t_v3	scale;
-	double	r;
-
-	scale = sphere->scale;
-	color = sphere->material.main;
-	printf("\033[0;31m-- Sphere Edition --\033[0m\nName: %s\n", sphere->name);
-	printf("Apply Translation to this position: (%lf, %lf, %lf)\n",
-		sphere->trans.x, sphere->trans.y, sphere->trans.z);
-	get_trans_vector(&translate);
-	translate = v3_add(sphere->trans, translate);
-	get_line_double("Radius scale (1 to keep)", &r);
-	get_line_color(&color);
-	sphere->material.main = color;
-	scale = v3_multi(r, scale);
-	transform_sphere(sphere, translate, (t_v3){0, 0, 0}, scale);
-}
-
-void	plan_edition(t_hittable *plan)
-{
-	t_color	color;
-	t_v3	translate;
-	t_v3	angles;
-
-	color = plan->material.main;
-	printf("\033[0;31m-- Plan Edition --\033[0m\nName: %s\n", plan->name);
-	printf("Apply Translation to this position: (%lf, %lf, %lf)\n",
-		plan->trans.x, plan->trans.y, plan->trans.z);
-	get_trans_vector(&translate);
-	translate = v3_add(plan->trans, translate);
-	angles = v3_multi(57.2957795131, plan->angles);
-	printf("> Current object world rotation angles x: %lf, y: %lf, z: %lf)\n",
-		angles.x, angles.y, angles.z);
-	get_line_angles(&angles);
-	get_line_color(&color);
-	plan->material.main = color;
-	transform_plane(plan, translate, angles, (t_v3){1, 1, 1});
-}
-
-void	start_edition(t_app *app)
-{
-	t_hittable	*obj;
-
-	obj = app->conf.selected_obj;
-	app->conf.status = 0;
-	if (obj->type == e_hit_sphere)
-		sphere_edition(obj);
-	else if (obj->type == e_hit_cylinder)
-		cylinder_edition(obj);
-	else if (obj->type == e_hit_plane)
-		plan_edition(obj);
-	app->conf.status = 0;
-	app->conf.selected_obj = NULL;
-	render(app);
-	app->conf.c_mode = e_normal_mode;
-	app->conf.rerender = true;
-	printf("\033[0;31m\n-- End --\033[0m\n");
-}
-
 void	start_selecting_mode(t_app *app, int x, int y)
 {
 	t_hittable	*obj;
 
 	if (app->conf.selected_obj != NULL)
-		start_edition(app);
+	{
+		printf("Object: %s, already selected \n", app->conf.selected_obj->name);
+		return;
+	}
 	else
 	{
 		obj = find_selected_object(app->scene, x, y);
 		if (obj)
-		{
 			app->conf.selected_obj = obj;
-			start_edition(app);
-		}
 		else
 			printf("No object found at that position, try again\n");
 	}
--- srcs/raytracing/plan.c
+++ srcs/raytracing/plan.c
@@ -24,7 +24,6 @@ void	compute_plan_hit(t_hit *hit)
 	n = plan->dir;
 	if (!hit->inside)
 		n = v3_multi(-1, plan->dir);
-	n = get_vector_tr(n, plan->tr, (t_v3){0, 0, 0});
 	hit->normal = normalize(n);
 	hit->acne_p = v3_add(hit->h_point, v3_multi(RAY_T_MIN, hit->normal));
 }
@@ -33,6 +32,8 @@ void	ft_compute_hit(t_hit *hit)
 {
 	if (hit->object->type == e_hit_sphere)
 		compute_sphere_hit(hit);
+	if (hit->object->type == e_hit_sphere_spec)
+		compute_transformable_sphere_hit(hit);
 	else if (hit->object->type == e_hit_cylinder)
 		compute_cylinder_hit(hit);
 	else if (hit->object->type == e_hit_plane)
@@ -43,13 +44,11 @@ void	intersect_plane(t_hit *hit, t_hittable *plan, t_ray *ray)
 {
 	double	d[2];
 	double	t;
-	t_ray	*s_ray;
 
-	s_ray = get_transformed_ray(ray, plan->inv_tr, (t_v3){0, 0, 0});
-	d[0] = ft_dot(plan->dir, s_ray->dir);
+	d[0] = ft_dot(plan->dir, ray->dir);
 	if (d[0] != 0)
 	{
-		d[1] = ft_dot(v3_sub((t_v3){0, 0, 0}, s_ray->o), plan->dir);
+		d[1] = ft_dot(v3_sub(plan->o, ray->o), plan->dir);
 		t = (d[1] / d[0]);
 		hit->t_trace[0] = t;
 		hit->t_trace[1] = RAY_T_MAX;
@@ -59,12 +58,11 @@ void	intersect_plane(t_hit *hit, t_hittable *plan, t_ray *ray)
 			hit->inside = true;
 			hit->intersection = true;
 			hit->h_point = get_point_on_ray_at(hit->t, hit->ray);
-			hit->h_point_obj_coord = get_point_on_ray_at(hit->t, s_ray);
-			if (ft_dot(plan->dir, normalize(s_ray->dir)) > 0)
+			hit->h_point_obj_coord = hit->h_point;
+			if (ft_dot(plan->dir, normalize(ray->dir)) > 0)
 				hit->inside = false;
 		}
 	}
-	free(s_ray);
 }
 
 bool	build_plane(t_scene *scn, t_point p, t_v3 normal, t_v3 v_color)
@@ -76,11 +74,16 @@ bool	build_plane(t_scene *scn, t_point p, t_v3 normal, t_v3 v_color)
 	if (plan)
 	{
 		plan->type = e_hit_plane;
-		plan->o = (t_v3){0, 0, 0};
+		plan->o = p;
 		plan->dir = normal;
 		color = make_color_vector(v_color, 1);
 		plan->material = build_default_material(color, 0.3, 0.7, 200);
-		transform_plane(plan, p, (t_v3){0, 0, 0}, (t_v3){1, 1, 1});
+		plan->trans = (t_v3){0, 0, 0};
+		plan->angles = (t_v3){0, 0, 0};
+		plan->scale = (t_v3){1, 1, 1};
+		plan->tr = get_identity_matrix();
+		plan->inv_tr = plan->tr;
+		plan->inv_tr_trans = plan->tr;
 		plan->name = add_name(scn, "Plan parsed o_n_", true);
 		ft_push(scn->hittable, plan);
 		return (true);
